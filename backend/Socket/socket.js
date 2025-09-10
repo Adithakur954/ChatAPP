4322bc98f1632146a9fc29e7ea72ca0f9c17b737
Aditya Singh <singhaditya1627@gmail.com>
2025-09-10 13:58:00 +0530

    first commit

---
+++ backend/Socket/socket.js
@@ -0,0 +1,37 @@
+import {Server} from 'socket.io';
+import http from 'http';
+import express from 'express';
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",           // for development
+      "https://slrtech-chatapp.onrender.com" // or your production frontend URL
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+
+export const getReciverSocketId = (receverId)=>{
+    return userSocketmap[receverId];
+};
+
+const userSocketmap={}; //{userId,socketId}
+io.on('connection',(socket)=>{
+    const userId = socket.handshake.query.userId;
+
+    if(userId !== "undefined") userSocketmap[userId] = socket.id;
+    io.emit("getOnlineUsers",Object.keys(userSocketmap))
+
+    socket.on('disconnect',()=>{
+        delete userSocketmap[userId],
+        io.emit('getOnlineUsers',Object.keys(userSocketmap))
+    });
+});
+
+export {app , io , server}
